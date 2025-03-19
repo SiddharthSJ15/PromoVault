3638869d1158a64853e2c3c07f25da2c2d4704b9
Siddharth Jayakumar <siddharthsj1011@gmail.com>
2025-03-19 17:20:53 +0530

    Added images, favorite stores icons,appbar icons

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 final List<String> imgList = [
@@ -8,6 +9,33 @@ final List<String> imgList = [
   'assets/images/shoe3.jpg',
   'assets/images/shoe4.jpg',
   'assets/images/shoe5.jpg',
+];
+
+final List<String> companyList = [
+  'assets/images/adidas.png',
+  'assets/images/puma.png',
+  'assets/images/nike.png',
+  'assets/images/fila.png',
+  'assets/images/skechers.png',
+  'assets/images/new-balance.png',
+  'assets/images/skechers.png',
+  'assets/images/skechers.png',
+  'assets/images/skechers.png',
+  'assets/images/skechers.png',
+  'assets/images/skechers.png',
+  'assets/images/skechers.png',
+];
+
+final List<String> companyNameList = [
+  'Adidas',
+  'Puma',
+  'Nike',
+  'Fila',
+  'Skechers',
+  'New \nBalance',
+  'Skechers',
+  'Skechers',
+  'Skechers',
 ];
 
 class HomePage extends StatefulWidget {
@@ -21,31 +49,106 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   automaticallyImplyLeading: false,
-      //   leading: Icon(FluentIcons.options_16_regular),
-      // ),
-      appBar: AppBar(),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: Builder(
+          builder: (context) {
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.shade200,
+                    ),
+                    child: IconButton(
+                      onPressed: () => Scaffold.of(context).openDrawer(),
+                      icon: Icon(Icons.clear_all_rounded, size: 20),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+
+      drawer: Drawer(
+        backgroundColor: Colors.grey.shade100,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.red),
+              child: Text('Drawer Header'),
+            ),
+
+            ListTile(
+              leading: Icon(FluentIcons.home_20_filled),
+              title: Text('Menu 1'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Menu 2'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(CupertinoIcons.home),
+              title: Text('Menu 3'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+
       body: Container(
         padding: EdgeInsets.all(10),
         child: Column(
           children: [
             Row(
               children: [
-                Expanded(child: TextFormField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey.shade100,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none
-                    )
+                Expanded(
+                  child: SizedBox(
+                    height: 40,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+
+                        fillColor: Colors.grey.shade200,
+                        prefixIcon: Icon(FluentIcons.search_32_regular),
+                        hintText: 'Search...',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding: EdgeInsets.all(10),
+                      ),
+                    ),
                   ),
-                )),
+                ),
+
                 SizedBox(width: 10),
-                SizedBox(child: Icon(FluentIcons.options_16_regular,))
+
+                SizedBox(
+                  width: 40,
+                  height: 40,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.shade200,
+                    ),
+                    child: Icon(FluentIcons.options_16_regular),
+                  ),
+                ),
               ],
             ),
+
+            SizedBox(height: 20),
+
             SizedBox(
               height: 200,
               child: ListView(
@@ -58,7 +161,7 @@ class _HomePageState extends State<HomePage> {
                       carouselImage('assets/images/shoe4.jpg'),
                     ],
                     options: CarouselOptions(
-                      height: 180,
+                      height: 170,
                       enlargeCenterPage: true,
                       autoPlay: true,
                       aspectRatio: 16 / 9,
@@ -69,6 +172,60 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ],
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Your favorite stores',
+                  style: TextStyle(fontFamily: 'GalanoGrotesqueMedium'),
+                ),
+              ],
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: List.generate(9, (index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 40,
+                            width: 40,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: Colors.grey.shade200,
+                              ),
+                              padding: EdgeInsets.all(5),
+                              child: Image.asset(
+                                companyList[index],
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          SizedBox(height: 20,
+                            child: Text(
+                              companyNameList[index],
+                              style: TextStyle(
+                                fontFamily: 'GalanoGrotesqueMedium',
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+                ),
               ),
             ),
           ],
